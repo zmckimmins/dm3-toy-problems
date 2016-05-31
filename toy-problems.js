df5c5d7a4eb1ef61cd0a7d4e69824d134791de08
mckmillions <zachmckimmins@gmail.com>
2016-05-31 09:29:44 -0500

    wk3-day2

--- toy-problems.js
+++ toy-problems.js
@@ -459,3 +459,58 @@ triangle(10);
 // }
 
 // makeTriangle();
+
+/*
+--------------
+Next Problem (Wk3-Day 2)
+--------------
+*/
+
+/*
+Write a generic function chainer
+Write a generic function chainer that takes a starting value, and an array of functions to execute on it.
+The input for each function is the output of the previous function (except the first function, which takes the starting value as it's input). Return the final value after execution is complete.
+*/
+function add(num) {
+  return num + 1
+}
+function mult(num) {
+  return num * 30
+}
+chain(2, [add, mult]);
+// returns 90;
+
+// Mentors solution
+// function chain(x, fns) {
+//   return fns.reduce(function(x, fn) {
+//     return fn(x)
+//   }, x)
+// }
+
+//Shakib's
+// function chain(start, arrOfFunc){
+//  var curr = arrOfFunc[0](start);
+//  for(var i=1; i<arrOfFunc.length; i++){
+//     curr = arrOfFunc[i](curr);
+//  }
+//  return curr;
+// }
+
+// Nic's
+// function chain(initialValue, array){
+//   num = initialValue;
+//   for(var i = 0; i < array.length; i++ ){
+//     num = array[i](num);
+//   }
+//   return num;
+// }
+
+// Dominic's
+function chain( val, funcs ) {
+    for (var i = 0; i < funcs.length; i++) {
+        val = funcs[i](val);
+    }
+    return val;
+}
+
+chain(2, [add, mult]) // 90
