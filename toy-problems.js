43d982974e560cdb630611b55161467f7bd1a2c1
mckmillions <zachmckimmins@gmail.com>
2016-06-20 10:19:46 -0500

    wk6-day1

--- toy-problems.js
+++ toy-problems.js
@@ -1402,6 +1402,83 @@ Next Problem (Wk6-Day 1)
 */
 
 /*
+Create a function named "rotate" that takes an array and returns a new one with the elements inside rotated n spaces.
+If n is greater than 0 it should rotate the array to the right. If n is less than 0 it should rotate the array to the left. If n is 0, then it should return the array unchanged.
+*/
+// var data = [1, 2, 3, 4, 5];
+
+// function rotate(array, n) {
+//   n = n % array.length
+//   return array.slice(-n).concat(array.slice(0, -n))
+// }
+
+// function rotate(array, n){
+// 	if( n === 0 ) return array;
+// 	var arr = array.slice();
+// 	if( n > 0 ) {
+// 		for ( var i = 0; i < n; i++){
+// 			arr.unshift(arr.pop());
+// 		}
+// 		return arr;
+// 	}
+// 	for ( var i = 0; i > n; i--){
+// 		arr.push(arr.shift());
+// 	}
+// 	return arr;
+// }
+
+// Nic's (not working yet)
+// function rotate(arr, num){
+//     let ans = [];
+//     if( num === 0 || num === 5){
+//         return arr;
+//     }else if(num > 0){
+//         num = num % arr.length;
+//         ans = arr.slice(num, arr.length);
+//         if (arr.length !== ans.length){
+//             ans.push(...arr.slice(0, num));
+//             return ans;
+//         }
+//     }
+
+// }
+
+// Dontavious
+// function rotate(data, num) {
+//   if (num > 0) {
+//     var last = [];
+//     last = data.pop();
+//     data.unshift(last);
+//     return data;
+//   } else if (num === 0) {
+//     return data;
+//   } else if (num < 0) {
+//     var first = [];
+//     first = data.shift();
+//     data.push(first);
+//     return data;
+//   }
+// }
+
+// console.log(rotate(data, 1)); // => [5, 1, 2, 3, 4]
+// console.log(rotate(data, 2)); // => [4, 5, 1, 2, 3]
+// console.log(rotate(data, 3)); // => [3, 4, 5, 1, 2]
+// console.log(rotate(data, 4)); // => [2, 3, 4, 5, 1]
+// console.log(rotate(data, 5)); // => [1, 2, 3, 4, 5]
+
+// console.log(rotate(data, 0)); // => [1, 2, 3, 4, 5]
+
+// console.log(rotate(data, -1)); // => [2, 3, 4, 5, 1]
+// console.log(rotate(data, -2)); // => [3, 4, 5, 1, 2]
+// console.log(rotate(data, -3)); // => [4, 5, 1, 2, 3]
+// console.log(rotate(data, -4)); // => [5, 1, 2, 3, 4]
+// console.log(rotate(data, -5)); // => [1, 2, 3, 4, 5]
+
+// console.log(rotate(data, 7));     // => [4, 5, 1, 2, 3]
+// console.log(rotate(data, 11));    // => [5, 1, 2, 3, 4]
+// console.log(rotate(data, 12478)); // => [3, 4, 5, 1, 2]
+
+/*
 --------------
 Next Problem (Wk6-Day 2)
 --------------
